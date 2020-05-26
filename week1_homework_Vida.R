4cb434d8c42492b792fc4dda993a951ce1c8b9c1
Fereshteh Sari <Fereshteh.sari@kcl.ac.uk>
2020-05-26 13:08:11 +0100

    Vida`s homework

--- week1_homework_Vida.R
+++ week1_homework_Vida.R
@@ -6,17 +6,52 @@
 
 # task 1: make a character vector named my_vector containing 5 elements
 
+my_vector <- c(1,2,3,4,5)
+my_vector <- c("hello", "goodmorning", "Streitlab", "bankholiday", "byebye")
+my_vector <- c(1, "hello", 3, 5, "streitlab")
+
+
+
 # task 2: make a matrix containing numbers 1:100 split across 10 columns
+matrix (data=1:100, ncol=10) 
+
+
 
 # Here is a test dataframe I have created from the metadata for my 10x scRNSseq data
 test <- read.csv("./test_data/week1_homework_scRNAseq_test.csv")
 
+
+
 # task 3: how many columns does the dataframe have?
+
+test.dataframe [rownum,colnum]=size(A)
+9
+
+
 
 # task 4: how many cells are female? Hint: you can use sum and the boolean operator to count TRUE values i.e. sum(x %in% y)
 
+"female" %in% test.dataframe [,9]
+test.dataframe[,9] %in% "female"
+test.datafarme sum([,9] %in% "female")
+
+
+
+
 # task 5: subset the rows which are male
 
+"male" %in% test.dataframe [,9]
+
+test.datafarme [,9] %in% "male"
+
+test.datafarme [test.dataframe [,9]%in% "male"]
+
+
+
+
 # task 6: subset a vector of the cell_IDs which are from stage hh4 (orig.ident column)
+
+cell_IDs <- c (test.datafarme[test.dataframe[,2] %in% "hh4",])
+
 
 
