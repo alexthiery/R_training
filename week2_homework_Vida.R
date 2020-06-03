7a719982dd7112c543cbc0b5d0f39c3ff675457f
Fereshteh Sari <Fereshteh.sari@kcl.ac.uk>
2020-06-03 06:55:22 +0100

    Week 2 work Vida

--- week2_homework_Vida.R
+++ week2_homework_Vida.R
@@ -1,6 +1,7 @@
 
 # here are two samples from a fake RNAseq dataset
 testdata <- readRDS("test_data/fakeRNAseq_week2.RDS")[1:2]
+sumary(testdata)
 
 # for this example I have replaced gene names with ensembl IDs to keep them unique
 # the data is stored as a list of dataframes
@@ -8,14 +9,30 @@ testdata <- readRDS("test_data/fakeRNAseq_week2.RDS")[1:2]
 
 # task 1: check whether ALL of the ensembl IDs in the first dataframe are present in the second
 
+testdata <- readRDS("test_data/fakeRNAseq_week2.RDS")[1:2]
+read.table("test_data/fakeRNAseq_week2.RDS")[1:2]
+testdata$SOX8_1[,"geneID"] %in% testdata$SOX8_2[,"geneID"]
+
+
 # task 2: 'intersect' the two dataframes to see which ensembl IDs are shared
 
+intersect(testdata$SOX8_1[,"geneID"], testdata$SOX8_2[,"geneID"])
+
 # task 3: use the vector of shared genes to subset both dataframes
+
+sharedgenes <- intersect(testdata$SOX8_1[,"geneID"], testdata$SOX8_2[,"geneID"])
+testdata$SOX8_1[testdata$SOX8_1[,'geneID'] %in% sharedgenes, ]
+testdata$SOX8_2[testdata$SOX8_2[,'geneID'] %in% sharedgenes, ]
 
 # task 4: use the 'order' function to re-order the second dataframe, based on the order of geneIDs in the first
 # HINT - look back at week 1 to see how to re-order a dataframe
 
+
+
+
 # task 5: now that your dataframes are in the same order combine the two dataframes into one
+
+
 
 
 ### Sami - below 'testdata' is a list with 7 samples instead of 2
