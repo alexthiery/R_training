5ab0143bea88d80b3ac18ff8750d2b518718d69b
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-06-16 18:54:55 +0100

    add answers for week3 and week4 homework

--- week3_homework_answers.R
+++ week3_homework_answers.R
@@ -9,7 +9,35 @@ testdata <- readRDS("./test_data/fakeRNAseq_week2.RDS")
 
 # 1) combine the dataframes using merge() - i.e. the easy way!
 
+for (i in names(testdata)) {
+  colnames(testdata[[i]])[2] <- paste0('counts_',i)
+}
+
+Merged.data<- testdata[[1]]
+for (i in names(testdata)[2:7]) {
+  Merged.data=merge(x=Merged.data, y=testdata[[i]], by='geneID')  
+  
+}
+
 # 2) combine using cbind() - this means you need to manually change the row order and subset shared rows
+
+Shared.genes<- testdata$SOX8_1$geneID [testdata$SOX8_1$geneID %in% testdata$SOX8_2$geneID &
+                                         testdata$SOX8_1$geneID %in% testdata$SOX8_3$geneID &
+                                         testdata$SOX8_1$geneID %in% testdata$control_1$geneID &
+                                         testdata$SOX8_1$geneID %in% testdata$control_2$geneID &
+                                         testdata$SOX8_1$geneID %in% testdata$control_3$geneID &
+                                         testdata$SOX8_1$geneID %in% testdata$control_4$geneID]
+
+newdata <- list()
+for (i in names(testdata)) {
+  colnames(testdata[[i]])[2] <- paste0('counts_',i)
+  newdata [[i]] <- testdata [[i]][testdata[[i]][, 'geneID'] %in% Shared.genes,]
+  newdata [[i]] <- newdata [[i]][order(newdata [[i]][,'geneID']),]
+}  
+
+newdata <- cbind(newdata[[1]], newdata[[2]], newdata[[3]], newdata[[4]],newdata[[5]], newdata[[6]], newdata[[7]])
+
+newdata <- newdata[,!duplicated(colnames(newdata))]
 
 # Good luck!
 
