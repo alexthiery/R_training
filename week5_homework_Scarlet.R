5a6befb5ef82b2b0064e05f98e31aa474ffaa4ea
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-06-16 19:24:26 +0100

    generate fake data instead of sox8 and edit in homework

--- week5_homework_Scarlet.R
+++ week5_homework_Scarlet.R
@@ -12,32 +12,11 @@ if(user == "Alex"){
 
 # bulk RNAseq differential expression analysis
 
-
 # list count files from alignment
-files <- list.files("./test_data/test_RNAseq/", full.names = T)
-# make dictionary of sample names and readcount files
-# samples are renamed from original sample sheet for downstream analysis. Original sample IDs can be found under Sample_ReadMe.txt
-samples.IDs <- data.frame(Sample = c("Sox8OE1", "Sox8OE2", "Sox8OE3", "Control1", "Control2", "Control3"),
-                          ID = c("WTCHG_706842_201108", "WTCHG_706842_202120", "WTCHG_706842_203132", "WTCHG_706842_205156",
-                                 "WTCHG_706842_207180", "WTCHG_706842_208192"),
-                          stringsAsFactors = F)
-
-
-# Here I am reading in the dataframes and assigning them to a list
-
-# read in the data tables for each sample, remove the extra columns which label chr pos etc, and add relevant column names
-# read in samples >> select columns for reads and annotations >> rename columns
-read.counts <- list()
-for(ID in samples.IDs$ID){
-  sample = samples.IDs[['Sample']][samples.IDs$ID %in% ID]
-  read.counts[[sample]] <- as.data.frame(read.table(files[grepl(ID, files)], header = T, stringsAsFactors = F))
-  read.counts[[sample]] <- read.counts[[sample]][,c('Geneid', "gene_name", colnames(read.counts[[sample]])[grepl(ID, colnames(read.counts[[sample]]))])]
-  names(read.counts[[sample]]) <- c('Gene.ID', 'Gene.ann', sample)
-}
-
+read.counts <- readRDS("~/dev/repos/R_training/test_data/test_DESeq2.RDS")
 
 # You can see that whereas in previous sessions we had two columns, we now have three: gene IDs, gene annotations and sample
-head(read.counts$Sox8OE1)
+head(read.counts$untreated1)
 
 # the aim is to create two dataframes from these data. First, we want a dataframe of the gene IDs and corresponding annotations. This is 
 # so that we can identify the ENS_ID for a gene of interest later. The second will be a dataframe of our gene counts.
